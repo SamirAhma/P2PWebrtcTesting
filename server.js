8954f867f9f081bbf051946d34c09b164a6182a7
SamirAhma <ahmaddin1000@gmail.com>
2020-10-16 21:47:46 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+const users = {};
+
+io.on('connection', socket => {
+    if (!users[socket.id]) {
+        users[socket.id] = socket.id;
+    }
+    socket.emit("yourID", socket.id);
+    io.sockets.emit("allUsers", users);
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+    })
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
+    })
+
+    socket.on("acceptCall", (data) => {
+        io.to(data.to).emit('callAccepted', data.signal);
+    })
+});
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    // All the javascript and css files will be read and served from this folder
+    app.use(express.static("client/build"));
+  
+    // index.html for all page routes    html or routing and naviagtion
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    });
+  }
+
+server.listen(8000, () => console.log('server is running on port 8000'));
+
+
